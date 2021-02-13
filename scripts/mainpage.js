c272aa70544be562978a91f107bf64da1607e996
kyle-pawlowski <kylepawlowski1@gmail.com>
2021-02-13 18:45:50 -0500

    add javascript

---
+++ scripts/mainpage.js
@@ -0,0 +1,14 @@
+const menu_item = document.querySelector('.contact');
+
+menu_item.style.backgroundColor = 'blue';
+
+<!--for(let i = 0; i < menu_item.length; i++){
+		
+    const single_item = menu_item[i];
+		single_item.style.backgroundColor = red;
+}
+
+menu_item.addEventListener('mouseover', function(){
+    menu_item.style.backgroundColor = pink;
+});--->
+
